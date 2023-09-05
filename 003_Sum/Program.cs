8e0a5e0e344bc26adc24967e01ff43ea079b6cd1
komarreks <komarreks@gmail.com>
2023-09-05 16:57:10 +0500

    add randon in 003 Sum

--- 003_Sum/Program.cs
+++ 003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int number1 = 3;
-int number2 = 4;
+﻿int number1 = new Random().Next(1,10); //123...9
+Console.WriteLine(number1);
+int number2 = new Random().Next(1,10); //123...9;
+Console.WriteLine(number2);
 int result = number1 + number2;
-console.WriteLine(result);
+Console.WriteLine(result);
