563f59ebcfaa5689bf91f9de374f237e367e78b6
komarreks <komarreks@gmail.com>
2023-09-05 17:07:07 +0500

    add 005 if - else

---
+++ 005_If_Else/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите имя пользователя: ");
+string name = Console.ReadLine();
+
+if(name.ToLower == "маша"){
+    Console.Write("Ура! Это же Маша!");
+}else{
+    Console.Write("Привет, "+name);
+}
